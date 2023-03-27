9b7706d183532a49b60e277c38e80c323ae7e501
AlexandreCoyras <alexandre.coyras@epitech.eu>
2023-03-27 13:29:35 +0200

    [ADD] contact animation scroll triggered

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,29 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import {useEffect, useState} from "react";
+gsap.registerPlugin(ScrollTrigger);
+
 
 
 export default function Contact() {
+
+    useEffect(() => {
+        gsap.to(".contact", {
+            scrollTrigger: {
+                trigger: ".contact",
+                start: "top center"
+            },
+            duration: 2,
+            translateY: 0,
+            ease: "back.out(1.4)",
+        });
+    }, [])
+
     return <>
         <div className={"h-screen flex"}>
-            <div className={"ml-14 md:ml-24 mt-24"}>
-                <div className={"text-4xl md:text-9xl pop "}>CONTACT</div>
-                <div className={"text-4xl md:text-9xl pop "}>ME</div>
+            <div className={"ml-14 md:ml-24 mt-24 "}>
+                <div className={"text-4xl md:text-9xl pop overflow-hidden"}><span className={"inline-block contact"} style={{transform: "translateY(100%)"}}>CONTACT</span></div>
+                <div className={"text-4xl md:text-9xl pop overflow-hidden"}><span className={"inline-block contact"} style={{transform: "translateY(100%)"}}>ME</span></div>
             </div>
         </div>
     </>
