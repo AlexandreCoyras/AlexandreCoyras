251a480516431bb9e263aa7d6112d7be38419534
AlexandreCoyras <alexandre.coyras@epitech.eu>
2023-03-28 20:06:06 +0200

    [FIX] github buttons bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,15 +71,15 @@ export default function Home() {
                 </a>
             </div>
 
-            <a href={"https://github.com/AlexandreCoyras/AlexandreCoyras"} target="_blank" rel="noopener noreferrer">
+            <a href={"https://github.com/AlexandreCoyras/AlexandreCoyras"} target="_blank" rel="noopener noreferrer" className={""}>
                 <div
-                    className="fixed bottom-8 right-20 md:right-24 bg-white text-black border-black rounded-full h-12 w-12 flex items-center justify-center hover:bg-black hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">
+                    className="z-40 fixed bottom-8 right-20 md:right-24 bg-white text-black border-black rounded-full h-12 w-12 flex items-center justify-center hover:bg-black hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">
                     <BiCode size={30}/>
                 </div>
             </a>
             <a href={"https://github.com/AlexandreCoyras"} target="_blank" rel="noopener noreferrer">
                 <div
-                    className="fixed bottom-8 right-6 md:right-8 bg-white text-black border-black rounded-full h-12 w-12 flex items-center justify-center hover:bg-black hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">
+                    className="z-40 fixed bottom-8 right-6 md:right-8 bg-white text-black border-black rounded-full h-12 w-12 flex items-center justify-center hover:bg-black hover:text-white transition-colors duration-200 ease-in-out cursor-pointer">
                     <AiFillGithub size={30}/>
                 </div>
             </a>
