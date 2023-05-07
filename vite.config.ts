0d4ef3f55462950eaf89cd982b14fb8add7ea2b5
AlexandreCoyras <alexandre.coyras@epitech.eu>
2023-05-07 23:02:40 +0200

    [CHANGE ALL] to vite react

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,7 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
-import pages from 'vite-plugin-pages';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  cacheDir: './node_modules/.vite/.',
-
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [
-    react(),
-    pages(),
-    viteTsConfigPaths({
-      root: './',
-    }),
-  ],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: './',
-  //    }),
-  //  ],
-  // },
-
-  test: {
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-  },
-});
+  plugins: [react()],
+})
