dcfdc66dcdf081a60c9ed373b3fc0872c4b9e8fc
AlexandreCoyras <alexandre.coyras@epitech.eu>
2023-05-07 23:32:19 +0200

    [FIX] vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import pages from 'vite-plugin-pages';
+import path from 'path';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: 'index.html',
-                pc: 'index.html',
+                main: './src/main.tsx',
+                ...getPagesInput('./src/pages'),
             },
         },
     },
     plugins: [react(), pages()],
 });
+
+function getPagesInput(pagesDir) {
+    const pages = {};
+
+    if (fs.existsSync(pagesDir)) {
+        const files = fs.readdirSync(pagesDir);
+
+        files.forEach((file) => {
+            const name = path.basename(file, path.extname(file));
+            pages[name] = `${pagesDir}/${file}`;
+        });
+    }
+
+    return pages;
+}
