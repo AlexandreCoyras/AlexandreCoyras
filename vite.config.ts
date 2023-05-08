ac2d33a58f779021b248444b064cb12456804ca2
AlexandreCoyras <alexandre.coyras@epitech.eu>
2023-05-08 12:23:58 +0200

    [MODIF ALL] to remix

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import pages from 'vite-plugin-pages';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react(), pages(), reactRefresh()],
-});
