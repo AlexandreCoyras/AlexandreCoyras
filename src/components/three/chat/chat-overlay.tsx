f1d1c93c9b984f04918db93230fe87e1b05728e4
AlexandreCoyras <alexandre.coyras@epitech.eu>
2023-12-21 15:00:05 -0500

    fix: chat bug

--- src/components/three/chat/chat-overlay.tsx
+++ src/components/three/chat/chat-overlay.tsx
@@ -92,66 +92,68 @@ const ChatOverlay = (props: any) => {
       <Suspense>
         <Dots loading={isPending} position={dotsPosition} />
       </Suspense>
-      <Html
-        transform
-        scale={0.01}
-        pointerEvents={clickedFirstScreen ? "auto" : "none"}
-        {...props}
-      >
-        <div className={"w-[125rem] h-[70.5rem] relative"}>
-          {data?.message?.content && (
-            <div className="absolute top-8 flex flex-col items-center justify-center left-1/2 transform -translate-x-1/2">
-              <SpeechBubble text={data?.message?.content} />
-            </div>
-          )}
-          <div className="fixed bottom-0 flex w-full mx-auto flex-col items-center space-y-3 p-5 pb-3 sm:px-0">
-            <form
-              ref={formRef}
-              onSubmit={handleSubmit}
-              className="relative w-full max-w-screen-md rounded-xl border border-gray-200 bg-background px-4 pb-2 pt-3 shadow-lg sm:pb-3 sm:pt-4"
-            >
-              <Textarea
-                ref={inputRef}
-                tabIndex={0}
-                required
-                rows={1}
-                autoFocus
-                placeholder="Send a message"
-                onChange={(e) => setInput(e.target.value)}
-                value={input}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter" && !e.shiftKey) {
-                    formRef.current?.requestSubmit()
-                    e.preventDefault()
-                  }
-                }}
-                spellCheck={false}
-                className="w-full pr-10 focus:outline-none bg-background"
-              />
-              <button
-                className={cn(
-                  "absolute inset-y-0 right-3 my-auto flex h-8 w-8 items-center justify-center rounded-md transition-all",
-                  disabled
-                    ? "cursor-not-allowed bg-background"
-                    : "bg-foreground/80  hover:bg-foreground"
-                )}
-                disabled={disabled}
+      <Suspense fallback={null}>
+        <Html
+          transform
+          scale={0.01}
+          pointerEvents={clickedFirstScreen ? "auto" : "none"}
+          {...props}
+        >
+          <div className={"w-[125rem] h-[70.5rem] relative"}>
+            {data?.message?.content && (
+              <div className="absolute top-8 flex flex-col items-center justify-center left-1/2 transform -translate-x-1/2">
+                <SpeechBubble text={data?.message?.content} />
+              </div>
+            )}
+            <div className="fixed bottom-0 flex w-full mx-auto flex-col items-center space-y-3 p-5 pb-3 sm:px-0">
+              <form
+                ref={formRef}
+                onSubmit={handleSubmit}
+                className="relative w-full max-w-screen-md rounded-xl border border-gray-200 bg-background px-4 pb-2 pt-3 shadow-lg sm:pb-3 sm:pt-4"
               >
-                {isPending ? (
-                  <LoadingCircle />
-                ) : (
-                  <SendIcon
-                    className={cn(
-                      "h-4 w-4",
-                      input.length === 0 ? "text-gray-700" : "text-background"
-                    )}
-                  />
-                )}
-              </button>
-            </form>
+                <Textarea
+                  ref={inputRef}
+                  tabIndex={0}
+                  required
+                  rows={1}
+                  autoFocus
+                  placeholder="Send a message"
+                  onChange={(e) => setInput(e.target.value)}
+                  value={input}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter" && !e.shiftKey) {
+                      formRef.current?.requestSubmit()
+                      e.preventDefault()
+                    }
+                  }}
+                  spellCheck={false}
+                  className="w-full pr-10 focus:outline-none bg-background"
+                />
+                <button
+                  className={cn(
+                    "absolute inset-y-0 right-3 my-auto flex h-8 w-8 items-center justify-center rounded-md transition-all",
+                    disabled
+                      ? "cursor-not-allowed bg-background"
+                      : "bg-foreground/80  hover:bg-foreground"
+                  )}
+                  disabled={disabled}
+                >
+                  {isPending ? (
+                    <LoadingCircle />
+                  ) : (
+                    <SendIcon
+                      className={cn(
+                        "h-4 w-4",
+                        input.length === 0 ? "text-gray-700" : "text-background"
+                      )}
+                    />
+                  )}
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-      </Html>
+        </Html>
+      </Suspense>
     </>
   )
 }
