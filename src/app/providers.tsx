db6cc117413a99e58e2d30180b8b685e97e078d5
AlexandreCoyras <alexandre.coyras@epitech.eu>
2023-12-23 21:00:09 -0500

    feat: analytics only in production

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,7 +7,7 @@ import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experime
 import posthog from "posthog-js"
 import { PostHogProvider } from "posthog-js/react"
 
-if (typeof window !== "undefined") {
+if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     capture_pageview: false, // Disable automatic pageview capture, as we capture manually
